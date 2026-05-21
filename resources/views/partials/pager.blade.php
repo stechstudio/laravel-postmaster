22888efa5b330506d54182a002995982f153de5a
Joseph Szobody <joseph@stechstudio.com>
2026-05-21 12:43:26 -0400

    Show 'Page X of Y' between the pagination buttons

--- resources/views/partials/pager.blade.php
+++ resources/views/partials/pager.blade.php
@@ -4,6 +4,9 @@
         @if ($paginator->previousPageUrl())
             <a class="pm-btn pm-btn--ghost pm-btn--sm" href="{{ $paginator->previousPageUrl() }}">← Prev</a>
         @endif
+        @if ($paginator->lastPage() > 1)
+            <span class="pm-page-of">Page {{ $paginator->currentPage() }} of {{ $paginator->lastPage() }}</span>
+        @endif
         @if ($paginator->nextPageUrl())
             <a class="pm-btn pm-btn--ghost pm-btn--sm" href="{{ $paginator->nextPageUrl() }}">Next →</a>
         @endif
